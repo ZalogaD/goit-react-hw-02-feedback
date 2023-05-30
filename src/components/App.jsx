a4cd2a719814c23735c2b44b71e42fb952251bbc
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-05-30 17:10:39 +0300

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import Statistics from './Statistics/Statistics';
 
 class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
   handleClick = (type) => {
     this.setState((prevState) => ({
       [type]: prevState[type] + 1
     }));
   };
 
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    if ( totalFeedback === 0 ) {
+      return 0;
+    }
+    return Math.round( (good/totalFeedback) * 100);
+  }
+
   render() {
     const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
 
     return (
-      <div>
-        <h1>Відгуки</h1>
-        <button onClick={() => this.handleClick('good')}>Добре</button>
-        <button onClick={() => this.handleClick('neutral')}>Нейтрально</button>
-        <button onClick={() => this.handleClick('bad')}>Погано</button>
-        <h2>Статистика:</h2>
-        <p>Добре: {good}</p>
-        <p>Нейтрально: {neutral}</p>
-        <p>Погано: {bad}</p>
-      </div>
+      <>
+    
+        <Statistics 
+                  good = {good}
+                  neutral = {neutral}
+                  bad = {bad}
+                  totalFeedback = {totalFeedback}
+                  positiveFeedbackPercentage = {positiveFeedbackPercentage}
+        />
+      </>
     );
   }
 }
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StatCont = styled.div`
+  width: 350px;
+`;
+
+export const StatList = styled.ul`
+  list-style: none;
+  margin: 0 auto;
+`
+export const StatItem = styled.li`
+  font-size: 18px;
+  color: #F0FFFF;
+  background-color: #2F4F4F;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+`
