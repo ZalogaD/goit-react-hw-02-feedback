bac368ac35f0dae0ec15ea839b7d8963052b6ecf
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-05-30 12:05:10 +0300

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  handleClick = (type) => {
+    this.setState((prevState) => ({
+      [type]: prevState[type] + 1
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h1>Відгуки</h1>
+        <button onClick={() => this.handleClick('good')}>Добре</button>
+        <button onClick={() => this.handleClick('neutral')}>Нейтрально</button>
+        <button onClick={() => this.handleClick('bad')}>Погано</button>
+        <h2>Статистика:</h2>
+        <p>Добре: {good}</p>
+        <p>Нейтрально: {neutral}</p>
+        <p>Погано: {bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
